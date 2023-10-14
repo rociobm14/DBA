5db7ff8aa9e9fe3012c6f482416ba4a0d137d322
Rorris <rociobarragan14@gmail.com>
2023-10-14 13:54:11 +0200

    switch menu works perfectly

--- Practica1/src/practica1/Practica1.java
+++ Practica1/src/practica1/Practica1.java
@@ -4,17 +4,89 @@
  */
 package practica1;
 
+import jade.core.Runtime;
+import jade.core.ProfileImpl;
+import jade.core.Profile;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import java.util.Scanner;
+
 /**
- *
- * @author rorris
+ * @author Rocío Barragán Moreno
+ * @description menú switch creado para elegir qué agente se va a ejecutar.
  */
+
 public class Practica1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        jade.core.Runtime rt = jade.core.Runtime.instance();
+        Profile p = new ProfileImpl();
+        p.setParameter(Profile.MAIN_HOST, "localhost"); // Cambia a tu dirección de host si es diferente
+        p.setParameter(Profile.CONTAINER_NAME, "Contenedor1"); // Puerto predeterminado de JADE
+        
+        ContainerController cc = rt.createAgentContainer(p);
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Introduzca el agente que se quiere ejecutar");
+        
+        int agente = scanner.nextInt();
+        
+        switch(agente){
+            case 1:
+                try {
+                    AgentController ac = cc.createNewAgent("rociobarragan", Ejercicio1.class.getCanonicalName(), null);
+                    ac.start();
+                } 
+        
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                
+                break;
+                
+            case 2:
+                try {
+                    AgentController ac = cc.createNewAgent("rociobarragan", Ejercicio2.class.getCanonicalName(), null);
+                    ac.start();
+                } 
+        
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                
+                break;
+                
+            case 3:
+                try {
+                    AgentController ac = cc.createNewAgent("rociobarragan", Ejercicio3.class.getCanonicalName(), null);
+                    ac.start();
+                } 
+        
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                
+                break;
+                 
+            case 4:
+                try {
+                    AgentController ac = cc.createNewAgent("rociobarragan", Ejercicio4.class.getCanonicalName(), null);
+                    ac.start();
+                } 
+        
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                
+                break;
+                
+            default:
+                System.out.println("No existe ese agente");        
+        }
+       
+    } 
 }
